cdbace2b4c57028234c07a5e4d3a927c7973a5ba
Vedran Petrac <vedran.petrac@infinum.com>
2024-02-20 08:53:51 +0100

    Update UpdateRevisionRequest.php

--- app/Http/Requests/UpdateRevisionRequest.php
+++ app/Http/Requests/UpdateRevisionRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreRevisionRequest extends FormRequest
+class UpdateRevisionRequest extends FormRequest
 {
     public function authorize(): bool
     {
